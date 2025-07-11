65d1789ac826edda6450187f1115a4136f200e3e
Kirill Svidunovich <thesuncatcher222@gmail.com>
2025-07-11 18:23:51 +0300

    feature(app): update imports

--- dodo_is_api_library/__init__.py
+++ dodo_is_api_library/__init__.py
@@ -1,7 +1,5 @@
-from dodo_is_api_library.dodo_is_api import (
-    DodoISApi,
-    DodoISScopes,
-)
+from dodo_is_api_library.dodo_is_api import DodoISApi
+from dodo_is_api_library.utils.scopes import DodoISScopes
 
 __all__ = (
     'DodoISApi',
