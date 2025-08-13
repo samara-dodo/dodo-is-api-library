7bcfd1ace20987212ed235d0a3e398833e78ff62
Kirill Svidunovich <thesuncatcher222@gmail.com>
2025-08-13 12:42:25 +0300

    feature(scopes): update DodoISScopes, refactor

--- dodo_is_api_library/utils/scopes.py
+++ dodo_is_api_library/utils/scopes.py
@@ -6,7 +6,11 @@ class DodoISScopes:
     Класс представления DodoIS Scopes.
     """
 
+    FRANCHISEE_READ = 'franchisee:read'      # ID франшизы
+    OFFLINE_ACCESS = 'offline_access'        # токен обновления
     OPENID = 'openid'                        # токен доступа
+    PHONE = 'phone'                          # номер телефона
+    PROFILE = 'profile'                      # профиль
     SALES = 'sales'                          # продажи
     STAFF_SHIFTS_READ = 'staffshifts:read'   # смены сотрудников / персонала, доступ на чтение
     USER_ROLE_READ = 'user.role:read'        # роли и юниты пользователя, доступ на чтение
@@ -21,4 +25,7 @@ class DodoISScopes:
         """
         missed_scopes: set[str] = required_scopes - set(user_scopes)
         if missed_scopes:
-            raise ValueError(f'У пользователя отсутсвуют обязательные scopes: {", ".join(missed_scopes)}')
+            raise ValueError(
+                'У пользователя отсутствуют обязательные scopes: '
+                f'{", ".join(missed_scopes)}',
+            )
