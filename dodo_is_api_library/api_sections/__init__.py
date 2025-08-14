55d4bc8f76f96498099f6283333d5cb8681c6fe2
Kirill Svidunovich <thesuncatcher222@gmail.com>
2025-08-14 18:02:43 +0300

    feature(app): register ApiOAuth

--- dodo_is_api_library/api_sections/__init__.py
+++ dodo_is_api_library/api_sections/__init__.py
@@ -1,7 +1,9 @@
 from dodo_is_api_library.api_sections.auth.auth import ApiAuth
 from dodo_is_api_library.api_sections.core.core import ApiCore
+from dodo_is_api_library.api_sections.auth.oauth import ApiOAuth
 
 __all__ = (
-    'ApiAuth',
-    'ApiCore',
+    "ApiAuth",
+    "ApiCore",
+    "ApiOAuth",
 )
