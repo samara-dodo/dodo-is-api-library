e146ab480443ac317dc0bf1a28d3e9ab3e8be866
Kirill Svidunovich <thesuncatcher222@gmail.com>
2025-08-14 18:02:22 +0300

    feature(app): add oauth, update protected args, update urls

--- dodo_is_api_library/dodo_is_api.py
+++ dodo_is_api_library/dodo_is_api.py
@@ -7,7 +7,9 @@ from typing import Callable
 from dodo_is_api_library.api_sections import (
     ApiAuth,
     ApiCore,
+    ApiOAuth,
 )
+from dodo_is_api_library.utils.exceptions import raise_http_exception
 
 
 class DodoISApi:
@@ -23,8 +25,8 @@ class DodoISApi:
             Секрет приложения в магазине приложений
         get_user_data: Callable
             Функция получения данных пользователя из сервиса
-        set_user_data: Callable
-            Функция сохранения данных пользователя в сервисе
+        update_user_data: Callable
+            Функция обновления данных пользователя в сервисе
         redirect_uri: str = 'https://localhost:5001/'
             URI для перенаправления после успешной авторизации в DodoIS
     """
@@ -34,28 +36,39 @@ class DodoISApi:
         client_id: str,
         client_secret: str,
         get_user_data: Callable,
-        set_user_data: Callable,
+        update_user_data: Callable,
         redirect_uri: str = 'https://localhost:5001/',
+        raise_http_exception: Callable = raise_http_exception,
     ):
         # Заполняются пользовательскими данными.
-        self.client_id: str = client_id
-        self.client_secret: str = client_secret
-        self.get_user_data: Callable = get_user_data
-        self.set_user_data: Callable = set_user_data
-        self.redirect_uri: str = redirect_uri
+        self.__client_id: str = client_id
+        self.__client_secret: str = client_secret
+        self.__get_user_data: Callable = get_user_data
+        self.__update_user_data: Callable = update_user_data
+        self.__redirect_uri: str = redirect_uri
+        self.__raise_http_exception: Callable = raise_http_exception
 
         # Заполняются автоматически.
         self.__base_url: str = 'https://api.dodois.io'
+        self.__base_url_oauth: str = 'https://auth.dodois.io'
 
         # Расширяемые классы.
         self.auth = ApiAuth(
-            client_id=client_id,
-            client_secret=client_secret,
-            get_user_data=self.get_user_data,
-            set_user_data=self.set_user_data,
-            redirect_uri=redirect_uri,
+            get_user_data=get_user_data,
+            raise_http_exception=raise_http_exception,
+            base_url=f"{self.__base_url}/auth",
         )
         self.core = ApiCore(
-            get_user_data=self.get_user_data,
-            base_url=self.__base_url,
+            get_user_data=self.__get_user_data,
+            raise_http_exception=raise_http_exception,
+            base_url=f"{self.__base_url}/dodopizza/ru",
+        )
+        self.oauth = ApiOAuth(
+            client_id=self.__client_id,
+            client_secret=self.__client_secret,
+            get_user_data=self.__get_user_data,
+            update_user_data=self.__update_user_data,
+            redirect_uri=self.__redirect_uri,
+            raise_http_exception=self.__raise_http_exception,
+            base_url=f"{self.__base_url_oauth}/connect",
         )
