f1c7bb39c2764df009c4b97436e368844b2f2fb8
Kirill Svidunovich <thesuncatcher222@gmail.com>
2025-08-14 17:55:00 +0300

    feature(app): register raise_http_exception

--- dodo_is_api_library/__init__.py
+++ dodo_is_api_library/__init__.py
@@ -1,7 +1,9 @@
 from dodo_is_api_library.dodo_is_api import DodoISApi
 from dodo_is_api_library.utils.scopes import DodoISScopes
+from dodo_is_api_library.utils.exceptions import raise_http_exception
 
 __all__ = (
     'DodoISApi',
     'DodoISScopes',
+    'raise_http_exception',
 )
