118fc47fff3684edae5305b28f8a703936602b6a
Kirill Svidunovich <thesuncatcher222@gmail.com>
2025-08-15 12:11:11 +0300

    feature(app): register ApiMarketplace

--- dodo_is_api_library/api_sections/__init__.py
+++ dodo_is_api_library/api_sections/__init__.py
@@ -1,9 +1,11 @@
 from dodo_is_api_library.api_sections.auth.auth import ApiAuth
 from dodo_is_api_library.api_sections.core.core import ApiCore
 from dodo_is_api_library.api_sections.auth.oauth import ApiOAuth
+from dodo_is_api_library.api_sections.marketplace.marketplace import ApiMarketplace
 
 __all__ = (
     "ApiAuth",
     "ApiCore",
     "ApiOAuth",
+    "ApiMarketplace",
 )
