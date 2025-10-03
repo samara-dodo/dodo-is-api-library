5320863186e312059bb4efc60dd1c945f4c7161b
Kirill Svidunovich <thesuncatcher222@gmail.com>
2025-10-03 17:09:52 +0300

    feature(app): add distributioncenters_get, integrate BusinessIds, CountryIds, UnitStates

--- dodo_is_api_library/api_sections/core/subsections/units.py
+++ dodo_is_api_library/api_sections/core/subsections/units.py
@@ -24,8 +24,11 @@ from dodo_is_api_library.utils.converter import (
 )
 from dodo_is_api_library.utils.scopes import DodoISScopes
 from dodo_is_api_library.utils.validators import (
+    BusinessIds,
+    CountryIds,
     process_full_address,
     process_legal_entity_name,
+    UnitStates,
 )
 
 class ApiUnits():
@@ -42,6 +45,142 @@ class ApiUnits():
         self.__get_user_data: Callable = get_user_data
         self.__raise_http_exception: Callable = raise_http_exception
         self.__base_url: str = f'{base_url}/units'
+
+
+    async def distributioncenters_get(
+        self,
+        business_id: str,
+        country_id: str,
+        organizations: Iterable[str] | None = None,
+        unit_states: Iterable[str] | None = None,
+        units: list[str] | None = None,
+        skip: int = 0,
+        take: int = 100,
+        take_all: bool = False,
+        user_id: Any = None,
+        user_data: dict[str, Any] | None = None,
+    ) -> list[dict[str, Any]]:
+        """
+        Заведения → Информация о ПРЦ
+
+        Возвращает список ПРЦ (производственно-распределительный центр) с информацией о них.
+
+        Документация: https://docs.dodois.io/docs/dodo-is/1bd039683b54a-zavedeniya-informacziya-o-pr-cz
+        URL: https://api.dodois.io/dodopizza/ru/units/distributioncenters
+
+        Для получения данных необходимо указывать параметр skip,
+        смещая его на количество уже полученных записей.
+        Повторять до тех пор, пока не будет достигнут конец списка
+        (isEndOfListReached = true).
+
+        Аргументы:
+            - business_id [str]: идентификатор бизнеса
+                                (для Пиццы - 63d4829611ea45c8ae71394860a2481c,
+                                 для Дринкит — c0b18e725258427a8bffea4f73957b0e)
+            - country_id [str]: идентификатор страны в формате ISO 3166-1 alpha-2
+            - organizations [Iterable[str]]: перечень ID организаций Dodo IS
+            - unit_states [Iterable[str]]: перечень состояний заведений в Dodo IS
+            - units [Iterable[str]]: список заведений (пиццерий) Dodo IS
+            - skip [int]: количество записей, которые следует пропустить
+            - take [int]: количество записей, которые следует выбрать
+            - take_all [bool]: признак, что нужно получить все записи из API
+
+        Требования к аргументам:
+            - в unit_states допустимы следующие значения: ["Open", "Close", "TemporaryClosed"]
+            - если take_all=True, то take будет проигнорирован
+
+        Требования к scopes:
+            - shared - общее
+        """
+        if user_data is None:
+            user_data = await self.__get_user_data(user_id=user_id)
+        self.__distributioncenters_get_validate_scopes(user_scopes=user_data['scopes'])
+        http_data: dict[str, Any] = self.__distributioncenters_get_http_params(
+            access_token=user_data['access_token'],
+            business_id=business_id,
+            country_id=country_id,
+            organizations=organizations,
+            unit_states=unit_states,
+            units=units,
+            skip=skip,
+            take=take,
+            take_all=take_all,
+        )
+        return_data: list[dict[str, Any]] = []
+        while 1:
+            status_, data, _ = await HttpClient.send_request(**http_data)
+            if status_ != HTTPStatus.OK:
+                self.__raise_http_exception(
+                    status_code=status_,
+                    detail=data,
+                )
+            return_data.extend(data["distributionCenters"])
+            if data['isEndOfListReached'] or not take_all:
+                break
+            else:
+                http_data['query_params']['skip'] += http_data['query_params']['take']
+        return self.__distributioncenters_get_process_data(data=return_data)
+
+    def __distributioncenters_get_process_data(
+        self,
+        data: list[dict[str, Any]],
+    ) -> list[dict[str, Any]]:
+        for d in data:
+            d["organizationName"] = process_legal_entity_name(d["organizationName"])
+            d["location"]["fullAddress"] = process_full_address(d["location"]["fullAddress"])
+        return data
+
+    def __distributioncenters_get_http_params(
+        self,
+        access_token: str,
+        business_id: str,
+        country_id: str,
+        organizations: Iterable[str] | None = None,
+        unit_states: Iterable[str] | None = None,
+        units: list[str | UUID] | None = None,
+        skip: int = 0,
+        take: int = 100,
+        take_all: bool = False,
+    ) -> dict[str, Any]:
+        """
+        Возвращает параметры HTTP запроса для distributioncenters_get.
+        """
+        BusinessIds.validate(value=business_id)
+        CountryIds.validate(value=country_id)
+        UnitStates.validate(value=unit_states)
+        return {
+            'method': HttpMethods.GET,
+            'url': f'{self.__base_url}/distributioncenters',
+            'query_params': {
+                k: v
+                for k, v
+                in {
+                    'businessId': business_id,
+                    'countryId': country_id,
+                    'organizations': ','.join(str(o) for o in organizations) if organizations else None,
+                    'unitStates': ','.join(str(s) for s in unit_states) if unit_states else None,
+                    'skip': 0 if take_all else skip,
+                    'take': 100 if take_all else take,
+                    'units': ','.join(convert_uuid_to_str(u) for u in units) if units else None,
+                }.items()
+                if v is not None
+            },
+            'headers': {'Authorization': f'Bearer {access_token}'},
+        }
+
+    def __distributioncenters_get_validate_scopes(
+        self,
+        user_scopes: Iterable[str],
+    ) -> None:
+        """
+        Проверяет наличие обязательных scopes для метода distributioncenters_get.
+        """
+        DodoISScopes.validate_scopes(
+            user_scopes=user_scopes,
+            required_scopes={
+                DodoISScopes.SHARED,
+            },
+        )
 
     async def shifts_get(
         self,
@@ -142,13 +281,6 @@ class ApiUnits():
         """
         if len(units) > 30:
             raise ValueError('В "units" можно перечислить до 30 заведений в одном запросе')
-        if clock_in_from:
-            clock_in_from = convert_datetime_to_str(clock_in_from)
-        if clock_in_to:
-            clock_in_to = convert_datetime_to_str(clock_in_to)
-        if take_all:
-            skip = 0
-            take = 100
         return {
             'method': HttpMethods.GET,
             'url': f'{self.__base_url}/shifts',
@@ -156,11 +288,11 @@ class ApiUnits():
                 k: v
                 for k, v
                 in {
-                    'from': clock_in_from,
-                    'to': clock_in_to,
+                    'from': convert_datetime_to_str(clock_in_from) if clock_in_from else None,
+                    'to': convert_datetime_to_str(clock_in_to) if clock_in_to else None,
                     'units': ','.join(convert_uuid_to_str(uid=u) for u in units),
-                    'skip': skip,
-                    'take': take,
+                    'skip': 0 if take_all else skip,
+                    'take': 100 if take_all else take,
                 }.items()
                 if v is not None
             },
@@ -279,17 +411,9 @@ class ApiUnits():
         """
         Возвращает параметры HTTP запроса для stores_get.
         """
-        if take_all:
-            skip = 0
-            take = 100
-        if (
-            unit_states is not None
-            and
-            not set(unit_states).issubset({"Open", "Close", "TemporaryClosed"})
-        ):
-            raise ValueError(
-                'В "unit_states" допустимы следующие значения: ["Open", "Close", "TemporaryClosed"]',
-            )
+        BusinessIds.validate(value=business_id)
+        CountryIds.validate(value=country_id)
+        UnitStates.validate(value=unit_states)
         return {
             'method': HttpMethods.GET,
             'url': f'{self.__base_url}/stores',
@@ -301,8 +425,8 @@ class ApiUnits():
                     'countryId': country_id,
                     'organizations': ','.join(str(o) for o in organizations) if organizations else None,
                     'unitStates': ','.join(str(s) for s in unit_states) if unit_states else None,
-                    'skip': skip,
-                    'take': take,
+                    'skip': 0 if take_all else skip,
+                    'take': 100 if take_all else take,
                     'units': ','.join(convert_uuid_to_str(u) for u in units) if units else None,
                 }.items()
                 if v is not None
