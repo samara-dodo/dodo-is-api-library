2b56155c6734f373fd7dcd6c7b1daddfe69cabbf
Kirill Svidunovich <thesuncatcher222@gmail.com>
2026-07-15 12:21:57 +0300

    feature(app): add convert_uuids_to_str

--- dodo_is_api_library/utils/converter.py
+++ dodo_is_api_library/utils/converter.py
@@ -3,6 +3,7 @@ from datetime import (
     datetime,
     timezone,
 )
+from typing import Iterable
 from uuid import UUID
 
 
@@ -41,3 +42,42 @@ def convert_uuid_to_str(
     if isinstance(uid, str):
         return uid.replace("-", "") if to_hex else uid
     return uid.hex if to_hex else str(uid)
+
+def convert_uuids_to_str(
+    uuids: Iterable[str | UUID],
+    to_hex: bool = True,
+    separator: str = ',',
+) -> str| None:
+    """Конвертирует список UUID в единую строку.
+
+    Принимает строго Iterable (list, tuple, set и т.д.), содержащий str или UUID.
+
+    Если to_hex=True, то возвращает строку в формате XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX,
+    иначе - строку в формате XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX.
+
+    В DodoIS API по-умолчанию используется HEX формат UUID.
+    """
+    if not uuids:
+        return None
+    if isinstance(uuids, str):
+        raise TypeError("Expected list/set/tuple, got str")
+
+    processed: list[str] = []
+    for item in uuids:
+        if not item:
+            continue
+        if isinstance(item, UUID):
+            processed.append(item.hex if to_hex else str(item))
+        elif isinstance(item, str):
+            clean_str = item.replace('-', '').strip()
+            if to_hex:
+                processed.append(clean_str)
+            else:
+                # INFO. Если нужен формат с дефисами, но строка может их
+                #       не содержать: безопасно восстановить значение через UUID.
+                processed.append(str(UUID(clean_str)))
+        else:
+            raise TypeError(
+                f"Expected str or UUID, got {type(item).__name__}",
+            )
+    return separator.join(processed) if processed else None
